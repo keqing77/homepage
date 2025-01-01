be5c186f4954acb3e1da2660721123c2b3af1314
keqing77 <lkay2333@gmail.com>
2025-01-01 22:05:56 +0800

    cicd: ignore typeErrors when building for this time

--- next.config.ts
+++ next.config.ts
@@ -4,15 +4,15 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-  /* config options here */
   devIndicators: {
     appIsrStatus: false,
   },
-  swcMinify: true,
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
   compress: true,
   poweredByHeader: false,
   reactStrictMode: true,
 };
 
-// export default nextConfig;
 export default withNextIntl(nextConfig);
